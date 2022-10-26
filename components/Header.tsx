4d48c3956d630d57d21cec44c94f99c6e9e2cd8e
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-10-26 14:40:16 +0900

    add/36:34

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+import { SearchIcon } from "heroicons/react/solid";
+
+function Header() {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Tv Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div>
+        <SearchIcon />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
